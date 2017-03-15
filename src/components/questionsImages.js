e9183df6a130106b8f95d9d098aa1def82c0dfe3
Derrick Sebesta <dsebesta@gmail.com>
2017-03-15 11:16:10 -0700

    input logic to advance screens, pull new images, and update title

--- src/components/questionsImages.js
+++ src/components/questionsImages.js
@@ -5,7 +5,7 @@ const QuestionImages = (props) => {
     console.log('props from questionImages', props.image);
     return (
         <div className={`col-lg-4 col-md-6 col-xs-12 + ${styles.questionImages}`}>
-            <img height="200" src={props.image}/>
+            <img height="200" src={props.image} onClick={props.increaseIndex}/>
         </div>
     )
 };
