c64c3e22b0eb5e5286b618792311d40087cbec23
Darin Estrin <darin.estrin@gmail.com>
2017-03-20 10:35:49 -0700

    Cleaned up and header and fixed routing inside header add google and facebook login function need to fix footer not displaying at the bottom of the page on index page

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
                 <a href="contact"><li>Contact Us</li></a>
             </ul>
         </div>
-        <div className='pull-6'>
+        <div className='text-center'>
             <p>Matchromonie</p>
             <p>9080 Irvine Center Dr Suite# 200</p>
             <p>Irvine, CA 92618</p>
