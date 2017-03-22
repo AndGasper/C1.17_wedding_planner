f782a24ba829958db20ce86cc8d9b5ad26800bc0
Darin Estrin <darin.estrin@gmail.com>
2017-03-21 18:55:37 -0700

    starting planner login form

--- src/components/plannerPageLogin.js
+++ src/components/plannerPageLogin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './app.css';
 import NavLink from './nav_link';
+import { reduxForm } from 'redux-form';
 
 const plannerPageLogin = () => (
     <div className = {styles.loginPage}>
