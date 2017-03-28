c781c421c582f4b9696b95282e09ce7e4f79a900
John Yosas <jayosas75@gmail.com>
2017-03-27 20:02:27 -0700

    fixed homepage for log'd in users

--- src/components/home.js
+++ src/components/home.js
@@ -29,11 +29,43 @@ class Home extends Component {
         console.log('yo this is props', this.props);
         this.props.handleProfileClick();
     }
-    render(){
 
-        return (
-            <div className="home">
-                <div className={`${styles.homePage}`} >
+    renderHomePage(){
+        const { authenticated } = this.props;
+        if(authenticated){
+            return (
+                <div key='home1' className={`${styles.homePage}`} >
+                    <div>
+                        <div className={`${styles.homeText}`}>
+                            <Paper zDepth={2} style={paperStyle}>
+                                <p>
+                                    Here at Matchromonie we aim to simplify planning your ideal wedding.
+                                    All it takes is five minutes of your time and we will find your wedding planner.
+                                </p>
+                                <p>
+                                    Don't want a wedding planner? No problem.
+                                    You can still use our site to help plan your needs.
+                                </p>
+                                <p>
+                                    How does it work? Instead of answering a bunch of forms we will show you an arrangement of pictures
+                                    and you decided which picture best represents how you imagine your wedding. No longer do you need to
+                                    browse multiple sites, searching for multiple wedding planners, and answering multiple boring forms,
+                                    trying to put dreams into words.
+                                </p>
+                            </Paper>
+                            <div>
+                                <Link to="/questions"><RaisedButton label='Retake Questionnaire' secondary={true} style={style}/></Link>
+                                <br />
+                                <RaisedButton onClick={this.handleProfile.bind(this)} label="Profile" secondary={true} style={style}/>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div key='home2' className={`${styles.homePage}`} >
                     <div>
                         <div className={`${styles.homeText}`}>
                             <Paper zDepth={2} style={paperStyle}>
@@ -56,13 +88,20 @@ class Home extends Component {
                                 <Link to="/questions"><RaisedButton label="Get Started" secondary={true} style={style}/></Link>
                                 <br />
                                 <Link to="/planner_login"><RaisedButton label="Wedding Planner?" secondary={true} style={style}/></Link>
-                                <br/>
-                                <RaisedButton onClick={this.handleProfile.bind(this)} label="Profile" secondary={true} style={style}/>
                             </div>
                         </div>
 
                     </div>
                 </div>
+                )
+        }
+    }
+
+    render(){
+
+        return (
+            <div className="home">
+                {this.renderHomePage()}
             </div>
         )
     }
